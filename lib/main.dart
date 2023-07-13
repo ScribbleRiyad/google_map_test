22c9ad57c8f0ae57d0c62d1ccdabc82ed26ea68b
ScribbleRiyad <scribbleriyad@gmail.com>
2023-07-13 18:21:33 +0600

    Map Test Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -39,53 +40,200 @@ class MapTest extends StatefulWidget {
 }
 
 class _MapTestState extends State<MapTest> {
+  String googleAPiKey = "AIzaSyD9J3lEnVen7qHXYRFcA8t4WhMA1Qb_F1w";
+
   GoogleMapController? mapController;
+  PolylinePoints polylinePoints = PolylinePoints();
+
   Set<Marker> markers = {}; //markers for google map
-  LatLng showLocation = const LatLng(23.822043, 90.427622);
+  Map<PolylineId, Polyline> polylines = {}; //polylines to show direction
+
+  LatLng startLocation = const LatLng(23.847864226127687, 90.4047250085373);
+  LatLng endLocation = const LatLng(23.751063667998775, 90.3903989601856);
+  // BitmapDescriptor markerbitmap = await BitmapDescriptor.fromAssetImage(
+  //   const ImageConfiguration(),
+  //   "asset/images/car.png",
+  // );
   @override
   void initState() {
     markers.add(Marker(
-      //add marker on google map
-      markerId: MarkerId(showLocation.toString()),
-      position: showLocation, //position of marker
+      //add start location marker
+      markerId: MarkerId(startLocation.toString()),
+      position: startLocation, //position of marker
       infoWindow: const InfoWindow(
         //popup info
-        title: 'Map',
-        snippet: 'Locations',
+        title: 'Starting Point ',
+        snippet: 'Start Marker',
       ),
       icon: BitmapDescriptor.defaultMarker, //Icon for Marker
     ));
 
-    //you can add more markers here
+    markers.add(Marker(
+      //add distination location marker
+      markerId: MarkerId(endLocation.toString()),
+      position: endLocation, //position of marker
+      infoWindow: const InfoWindow(
+        //popup info
+        title: 'Destination Point ',
+        snippet: 'Destination Marker',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    getDirections(); //fetch direction polylines from Google API
+
     super.initState();
+  }
+
+  getDirections() async {
+    List<LatLng> polylineCoordinates = [];
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleAPiKey,
+      PointLatLng(startLocation.latitude, startLocation.longitude),
+      PointLatLng(endLocation.latitude, endLocation.longitude),
+      travelMode: TravelMode.transit,
+    );
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    } else {
+      print(result.errorMessage);
+    }
+    addPolyLine(polylineCoordinates);
+  }
+
+  addPolyLine(List<LatLng> polylineCoordinates) {
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.black,
+      points: polylineCoordinates,
+      width: 4,
+    );
+    polylines[id] = polyline;
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         title: const Center(
           child: Text('Google Map'),
         ),
       ),
-      body: GoogleMap(
-        //Map widget from google_maps_flutter package
-        zoomGesturesEnabled: true, //enable Zoom in, out on map
-        initialCameraPosition: CameraPosition(
-          //innital position in map
-          target: showLocation, //initial position
-          zoom: 8.0, //initial zoom level
-        ),
-        markers: markers, //markers to show on map
-        mapType: MapType.normal, //map type
-        onMapCreated: (controller) {
-          //method called when map is created
-          setState(() {
-            mapController = controller;
-          });
-        },
-      ),
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade100,
+                          borderRadius: BorderRadius.circular(10)),
+                      height: MediaQuery.of(context).size.height * 0.34,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 4, left: 5, right: 5),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)),
+                              height: MediaQuery.of(context).size.height * 0.2,
+                              child: GoogleMap(
+                                polylines: Set<Polyline>.of(polylines.values),
+                                myLocationButtonEnabled: true,
+                                myLocationEnabled: true,
+                                zoomGesturesEnabled: true,
+                                // trafficEnabled: true,
+                                markers: markers,
+                                mapType: MapType.normal,
+                                initialCameraPosition: CameraPosition(
+                                  target: startLocation,
+                                  zoom: 11.0,
+                                ),
+                                onMapCreated: (controller) {
+                                  setState(() {
+                                    mapController = controller;
+                                    getDirections();
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                          width: 80.00,
+                                          height: 60.00,
+                                          decoration: BoxDecoration(
+                                              color: Colors.grey.shade300,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(20.0),
+                                            child: Image.asset(
+                                                'asset/images/image.png'),
+                                          )),
+                                      Column(
+                                        children: const [
+                                          Text("Airport Rental"),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text("July 7-12.45 Am"),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text("BDT 50000"),
+                                          SizedBox(
+                                            height: 5,
+                                          )
+                                        ],
+                                      ),
+                                      Container(
+                                          width: 100.00,
+                                          height: 60.00,
+                                          decoration: BoxDecoration(
+                                              color: Colors.grey.shade300,
+                                              borderRadius:
+                                                  BorderRadius.circular(10)),
+                                          child: const Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: Center(
+                                                  child: Text("Pending")))),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )),
     );
   }
 }
